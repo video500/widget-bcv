ab51a92995008ed32085d314930ddd5f1df947d6
Manuel Jerez <alejandrojerezc@yahoo.com>
2025-04-27 21:36:15 -0400

    Modificado curl para ser silencioso.

--- go.sh
+++ go.sh
@@ -1,6 +1,6 @@
 cd ~/widget-bcv
-curl -o bcv.html --cacert bcv-org-ve.pem https://www.bcv.org.ve
-curl -o telegram_dolar.html https://t.me/s/monitornoticiasvzla
+curl -s -o bcv.html --cacert bcv-org-ve.pem https://www.bcv.org.ve
+curl -s -o telegram_dolar.html https://t.me/s/monitornoticiasvzla
 python widget-bcv.py | termux-notification -t "Dólar hoy"
 
 
